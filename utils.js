8f486a5c7b88569191d065bbab6bdc8109eab040
Michael Kovacs <michael@joinpakt.com>
2022-04-26 14:35:12 -0700

    script to allow for grabbing URL params and get/set a cookie and building a URL param string for decorating links on a page

---
+++ utils.js
@@ -0,0 +1,37 @@
+function bake_cookie(name, value) {
+  let cookie = [name, '=', JSON.stringify(value), '; domain=.', window.location.host.toString(), '; path=/;'].join('');
+  document.cookie = cookie;
+}
+
+function read_cookie(name) {
+  let result = document.cookie.match(new RegExp(name + '=([^;]+)'));
+  result && (result = JSON.parse(result[1]));
+  return result;
+}
+
+function build_params(params) {
+  let result = "";
+  let keys = Object.keys(params);
+  for (const k of keys) {
+    result += k + "=" + params[k] + "&";
+  }
+  result = result.slice(0, -1);
+  return result;
+}
+
+function get_cookied_url_params(cookieName) {
+
+  let cookieVal = read_cookie(cookieName);
+  if(!cookieVal) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let entries = urlParams.entries();
+    let obj = {};
+    for(const entry of entries) {
+      obj[entry[0]] = entry[1];
+    }
+    bake_cookie(cookieName, obj);
+    cookieVal = obj;
+  }
+  return build_params(cookieVal);
+}
